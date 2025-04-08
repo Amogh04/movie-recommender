39ee638f01acc880a8a97ff770691139200c7b1f
Amogh Jain <amoghjain04@gmail.com>
2025-04-08 21:44:59 +0530

    Deployed on render

--- app.py
+++ app.py
@@ -4,15 +4,22 @@ import pandas as pd
 import pickle
 import os
 from dotenv import load_dotenv
-from vectorize import similarity
+# from vectorize import get_similarity
+    
     
 load_dotenv()
 api_key = os.getenv("api_key")
-	
-app = Flask(__name__)
+    
+app = Flask(__name__, template_folder="templates", static_folder="static")
+    
     
 movies_data = pickle.load(open("data.pkl","rb"))
-	
+similarity = pickle.load(open("similarity.pkl","rb"))
+
+    
+# similarity = get_similarity()
+# similarity = []
+    
 def recommend_movies(movie):
     movie = movie.lower()
     movie_index = movies_data[movies_data['title'] == movie.lower()]
@@ -22,18 +29,16 @@ def recommend_movies(movie):
     all_movie_list = sorted(list(enumerate(alikes)), reverse=True, key = lambda x:x[1])[1:6]
     movie_list = []
     for i in all_movie_list:
-    	movie_id = movies_data.iloc[i[0]].id
-    	url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}"
-    	response = requests.get(url)
-    	data = response.json()
-    	poster_path = data.get("poster_path")
-    	if poster_path:
-    		image_url = f"https://image.tmdb.org/t/p/w500{poster_path}"
-    		print("Poster URL:", image_url)
-    	else:
+        movie_id = movies_data.iloc[i[0]].id
+        url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}"
+        response = requests.get(url)
+        data = response.json()
+        poster_path = data.get("poster_path")
+        if poster_path:
+            image_url = f"https://image.tmdb.org/t/p/w500{poster_path}"
+        else:
             image_url=''
-            print("No poster found.")
-    	movie_list.append({'title':movies_data.iloc[i[0]].title.capitalize(),'thumb':image_url})
+        movie_list.append({'title':movies_data.iloc[i[0]].title.capitalize(),'thumb':image_url})
     return movie_list
 
 @app.route("/", methods=["GET", "POST"])
@@ -46,12 +51,13 @@ def index():
 @app.route("/show")
 def show():
     movie = request.args.get('movie')
+    movie = movie.replace("+", " ")
+    # similarity = get_similarity()
     recommendations = recommend_movies(movie)
     return render_template("movies.html", movie=movie, recommendations=recommendations)
-
+    
 if __name__ == "__main__":
-	app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
-#Vercel
-def handler(environ, start_response):
-    return app.wsgi_app(environ, start_response)
+    
